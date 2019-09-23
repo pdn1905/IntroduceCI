595950c0fe41245701f6faf0942883dbbbdc666b
phan.duong.ngoc <phan.duong.ngoc@framgia.com>
2019-09-23 14:53:51 +0700

    init commit

---
+++ IntroduceCI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IntroduceCI
+//
+//  Created by phan.duong.ngoc on 9/23/19.
+//  Copyright © 2019 phan.duong.ngoc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
